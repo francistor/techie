e08d648f004e18710a0f6d3f9a37c438d06b625e
francistor <francisco.cardosogil@gmail.com>
2021-06-05 18:18:04 +0200

    Delete add_node.sh

--- add_node.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-node_name=$1
-echo adding node $node_name
-
-ssh $node_name "rm -rf techie && git clone https://github.com/francistor/techie.git && cd techie && ./k8s_preinstall_node.sh"
