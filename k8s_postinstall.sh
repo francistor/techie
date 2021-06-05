b61a3229086649e02b10b83ab59287380b0039cf
francistor <francisco.cardosogil@gmail.com>
2021-06-05 15:36:23 +0200

    Create k8s_postinstall.sh

---
+++ k8s_postinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+# Create kubeconfig file
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  
+# Configure usage of Calico CNI
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+  
+# Untaint master, so that it will accomodate workoads also
+K8S_MASTER=$(kubectl get nodes | awk '$3~/master/'| awk '{print $1}')
+kubectl taint node $K8S_MASTER node-role.kubernetes.io/master:NoSchedule-
