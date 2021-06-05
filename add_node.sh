5d4cc720344bf57793a08fb518362119ae2812b8
francistor <francisco.cardosogil@gmail.com>
2021-06-05 18:09:40 +0200

    Update add_node.sh

--- add_node.sh
+++ add_node.sh
@@ -3,4 +3,4 @@
 node_name=$1
 echo adding node $node_name
 
-ssh $node_name "git clone https://github.com/francistor/techie.git && cd techie && ./k8s_preinstall_node.sh"
+ssh $node_name "rm -rf techie && git clone https://github.com/francistor/techie.git && cd techie && ./k8s_preinstall_node.sh"
