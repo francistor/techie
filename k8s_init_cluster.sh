fe580444128eb51777c1c40e50c609ee7f771e8d
francistor <francisco.cardosogil@gmail.com>
2021-06-05 17:48:15 +0200

    Update k8s_init_cluster.sh

--- k8s_init_cluster.sh
+++ k8s_init_cluster.sh
@@ -1,1 +1,1 @@
-sudo -c "kubeadm init --apiserver-advertise-address=192.168.122.2 --pod-network-cidr=10.251.0.0/16" > join_cluster.txt
+sudo kubeadm init --apiserver-advertise-address=192.168.122.2 --pod-network-cidr=10.251.0.0/16 > join_cluster.txt
