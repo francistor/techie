eeba512bf195306fc5430217223f22338c83e38a
francistor <francisco.cardosogil@gmail.com>
2021-06-05 16:04:28 +0200

    Update k8s_postinstall.sh

--- k8s_postinstall.sh
+++ k8s_postinstall.sh
@@ -11,3 +11,6 @@ kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 # Untaint master, so that it will accomodate workoads also
 K8S_MASTER=$(kubectl get nodes | awk '$3~/master/'| awk '{print $1}')
 kubectl taint node $K8S_MASTER node-role.kubernetes.io/master:NoSchedule-
+
+# Enable iSCSI for OpenEBS
+sudo systemctl enable --now iscsid
