68b2050b3cfa7ce28c17494746139297a2ecf5f5
francistor <francisco.cardosogil@gmail.com>
2021-08-22 15:05:09 +0200

    Update k8s_postinstall_master.sh

--- k8s-installer/k8s_postinstall_master.sh
+++ k8s-installer/k8s_postinstall_master.sh
@@ -53,5 +53,5 @@ data:
       - $METALLB_IP_RANGE" | kubectl apply -f -
       
 # Install Ingress controller
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.46.0/deploy/static/provider/baremetal/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.49.0/deploy/static/provider/baremetal/deploy.yaml
 
